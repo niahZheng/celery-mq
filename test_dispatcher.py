7eaf3cd1764754fc13972d62f620592d198ed0a3
walter-2025-zhao <wpz_2025@sina.com>
2025-06-23 10:16:08 +0800

    conversationid

--- test_dispatcher.py
+++ test_dispatcher.py
@@ -26,12 +26,13 @@ def send_test_message():
     test_id = "cda1ce9a-af1e-499d-897c-e82da9c165e5"
     test_type="session_ended" # "session_started" "session_ended" "transcription"
     # 构建任务参数 
-    topic = f"agent-assist/{test_id}/{test_type}"
+    topic = f"agent-assist/{test_id}/{test_type}" 
+
     # 测试消息
     test_message = {
         "type": test_type, # "session_ended"
-        "conversationid": test_id,
         "parameters": {
+            "conversationid": test_id,
             "session_id": test_id,
             "conversationStartTime": "2025-06-19 03:37:21.533123",
             "conversationEndTime": None,
@@ -39,21 +40,21 @@ def send_test_message():
         },
     }
     # test_message = {
-    #     "type": test_type, # "transcription"
-    #     "conversationid": test_id,
+    #     "type": test_type, # "transcription" 
     #     "parameters": {
+    #         "conversationid": test_id,
     #         "session_id": test_id,
     #         "source": "external",
     #         # "text": "so you're um already extended you're a pretire it will end on august 24 so you need to give us a call back before always 20 fourth to give us a feedback if you i mean what happened to the product if it didn't something you know it give you the benefits such in it but let's say uh you love the product you like the product you don't need to give us a call back then it will be automatically uh you will be receiving a not a bottle for your subscription and uh set up a call i heard that you're not you know um sure uh you're dilling about the price which is $83 and 8 right okay so what i can do for you is aside from extending the your pre trial for another 15 days i'm also giving you my employee discount which is 20 percent of the scott so instead of paying $83 and 8 sets going to pay only $67.04 how about that",
     #         "text": "so you're um already extended you're a pretire it will end on august 24 so you need to give us a call back "+ str(datetime.now()),
     #         "seq": None,
-    #         "timestamp": 68.66, 
+    #         "timestamp": datetime.now().timestamp(), #68.66, 
     #     },
     # }
     # test_message = {
     #     "type": test_type, # "session_started"
-    #     "conversationid": test_id,
     #     "parameters": {
+    #         "conversationid": test_id,
     #         "session_id": test_id,
     #         "conversationStartTime": "2025-06-19 03:37:21.533123",
     #         "conversationEndTime": None,
